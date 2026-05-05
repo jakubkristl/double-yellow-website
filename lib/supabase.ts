9400b291bb305735b9d3945b83198511617053dc
Jakub Kristl <jakubkristl77@gmail.com>
2026-05-05 19:49:55 +0300

    auto: 2026-05-05 16:49:55

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -29,11 +29,13 @@ export type Database = {
   };
 };
 
-let _client: SupabaseClient<Database> | null = null;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let _client: SupabaseClient<any> | null = null;
 
-export function createBrowserClient(): SupabaseClient<Database> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function createBrowserClient(): SupabaseClient<any> {
   if (!_client) {
-    _client = createClient<Database>(
+    _client = createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     );
